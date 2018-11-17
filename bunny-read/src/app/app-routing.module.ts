b18a80360e829bc05ce8ad59e251e181375562e1
CaoFang98 <213161650@seu.edu.cn>
2018-11-18 01:56:18 +0800

    luodao

--- bunny-read/src/app/app-routing.module.ts
+++ bunny-read/src/app/app-routing.module.ts
@@ -5,14 +5,13 @@ import { ContentComponent } from './content/content.component';
 import { CommentComponent } from './comment/comment.component';
 import { MarkedContentComponent } from './marked-content/marked-content.component';
 import { SettingsComponent } from './settings/settings.component';
-import { LoginWindowComponent } from './login-window/login-window.component'
+
 const routes: Routes = [
   { path: '', redirectTo: '/content', pathMatch: 'full' },
   { path: 'content', component:  ContentComponent },
   { path: 'marked-content/:anchor', component: MarkedContentComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'comment', component: CommentComponent },
-  { path: 'login', component: LoginWindowComponent }
+  { path: 'comment', component: CommentComponent }
 ];
 
 @NgModule({
